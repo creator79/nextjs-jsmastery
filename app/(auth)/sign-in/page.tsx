b6a9ed0275a70ddce3b4bc928cb06468cfc30298
creator79 <upadhyayvivek.19@gmail.com>
2024-01-24 01:19:54 +0530

    group routes

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import Link from 'next/link'
+
+
+export default function SignIn() {
+  return (
+    <main>
+      
+      <h1>Welcome to Sign In Page</h1>
+
+    </main>
+  );
+}
